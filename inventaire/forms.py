7be49e7e2aa00ce632831f837dd5be51890e3545
megtaho <122614093+megtaho@users.noreply.github.com>
2025-04-24 16:05:55 +0200

    ajout formulaire

---
+++ inventaire/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Produit
+
+# Formulaire pour ajouter/modifier un produit
+class ProduitForm(forms.ModelForm):
+    class Meta:
+        model = Produit  # Spécifie le modèle associé au formulaire
+        fields = ['nom', 'description', 'prix', 'quantite_stock']  # Les champs à inclure dans le formulaire
