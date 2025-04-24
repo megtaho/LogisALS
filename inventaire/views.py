e6703642e57adb708abdd4e2f93008837e33a2ec
megtaho <122614093+megtaho@users.noreply.github.com>
2025-04-24 11:22:32 +0200

    creation vues pour interagir avec modeles

---
+++ inventaire/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from .models import Produit
+
+# Vue pour afficher tous les produits
+def liste_produits(request):
+    produits = Produit.objects.all()  # Récupère tous les produits
+    return render(request, 'inventaire/liste_produits.html', {'produits': produits})
+
+
+from .models import Commande
+
+def liste_commandes(request):
+    commandes = Commande.objects.all()  # Récupère toutes les commandes
+    return render(request, 'inventaire/liste_commandes.html', {'commandes': commandes})
+
+# inventaire/views.py
+from .models import Rapport
+
+def liste_rapports(request):
+    rapports = Rapport.objects.all()  # Récupère tous les rapports
+    return render(request, 'inventaire/liste_rapports.html', {'rapports': rapports})
+
