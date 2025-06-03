a1775e9accd0a095ab4dd047f2cd8d6ea8bbd3da
megtaho <122614093+megtaho@users.noreply.github.com>
2025-06-03 18:03:25 +0200

    ajout champs vendeur + statut

--- inventaire/urls.py
+++ inventaire/urls.py
@@ -12,9 +12,18 @@ urlpatterns = [
 
     # Route pour afficher les rapports
     path('rapports/', views.rapport_ventes, name='rapport_ventes'),
+    path('rapports/complet/', views.rapport_complet, name='rapport_complet'),
+
 
     path('produit/ajouter/', views.ajouter_produit, name='ajouter_produit'),
     path('produit/modifier/<int:pk>/', views.modifier_produit, name='modifier_produit'),
     path('produit/supprimer/<int:pk>/', views.supprimer_produit, name='supprimer_produit'),
+    
+
+    path('commande/<int:pk>/modifier-statut/', views.modifier_statut_commande, name='modifier_statut_commande'),
+    path('stocks/', views.gestion_stock, name='gestion_stock'),
+
+    path('a-propos/', views.a_propos, name='a_propos'),
+
 
 ]
